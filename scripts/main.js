54dcf6dfbea2221d44e6f7ef8584632b12c67f48
moicenti <00054123@uca.edu.sv>
2026-02-01 01:19:31 -0600

    EVEN MORE DEBUGGIN

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,30 @@ import { AudioZoneApp } from './zone-app.js';
 console.log('Audio Zones | main.js cargado');
 
 Hooks.on('renderSidebar', (sidebar, html) => {
-  console.log('renderSidebar hook disparado');
+try{
+  console.log('Audio Zones | renderSidebar disparado');
 
   if (html.find('#audio-zones-button').length){ 
-    console.log("ggs");
+    console.log('Audio Zones | se encontro el boton ya creado');
     return;}
-console.log("duro")
+console.log("d'Audio Zones | El boton no estbaa creado'")
   const button = $(`
     <button id="audio-zones-button" class="audio-zones-btn">
       <i class="fas fa-volume-up"></i>
     </button>
   `);
 
-  console.log('botón creado');
+  console.log('Audio Zones | boton creado');
 
   button.click(() => {
-    console.log('botón clickeado');
+    console.log('Audio Zones | boton pulsado');
     new AudioZoneApp().render(true);
   });
 
   html.find('#sidebar-tabs').append(button);
-  console.log('botón insertado en sidebar');
+  console.log('Audio Zones | boton insertado');
+}catch(error){
+    console.log("Audio zone | valio verga "+error)
+}
+
 });
