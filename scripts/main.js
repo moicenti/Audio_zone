343bdea6b1de391afb794f4ad0d3f2b888078e29
moicenti <00054123@uca.edu.sv>
2026-02-01 01:24:46 -0600

    idk

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,35 @@
-import { AudioZoneApp } from './zone-app.js';
+import { AudioZoneApp } from "./zone-app.js";
 
-console.log('Audio Zones | main.js cargado');
+console.log("Audio Zones | main.js cargado");
 
-Hooks.on('renderSidebar', (sidebar, html) => {
-try{
-  console.log('Audio Zones | renderSidebar disparado');
+Hooks.on("renderSidebar", (sidebar, html) => {
+  try {
+    const $html = $(html);
+    console.log("Audio Zones | " + html);
+    console.log("Audio Zones | " + $html);
+    console.log("Audio Zones | renderSidebar disparado");
 
-  if (html.find('#audio-zones-button').length){ 
-    console.log('Audio Zones | se encontro el boton ya creado');
-    return;}
-console.log("d'Audio Zones | El boton no estbaa creado'")
-  const button = $(`
+    if ($html.find("#audio-zones-button").length) {
+      console.log("Audio Zones | se encontro el boton ya creado");
+      return;
+    }
+    console.log("d'Audio Zones | El boton no estbaa creado'");
+    const button = $(`
     <button id="audio-zones-button" class="audio-zones-btn">
       <i class="fas fa-volume-up"></i>
     </button>
   `);
 
-  console.log('Audio Zones | boton creado');
+    console.log("Audio Zones | boton creado");
 
-  button.click(() => {
-    console.log('Audio Zones | boton pulsado');
-    new AudioZoneApp().render(true);
-  });
+    button.click(() => {
+      console.log("Audio Zones | boton pulsado");
+      new AudioZoneApp().render(true);
+    });
 
-  html.find('#sidebar-tabs').append(button);
-  console.log('Audio Zones | boton insertado');
-}catch(error){
-    console.log("Audio zone | valio verga "+error)
-}
-
+    $html.find("#sidebar-tabs").append(button);
+    console.log("Audio Zones | boton insertado");
+  } catch (error) {
+    console.log("Audio zone | valio verga " + error);
+  }
 });
